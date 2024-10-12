bb724a9e4b7762ae9afeab416accbf2047570e97
vaniog <vaniog@yandex-team.ru>
2024-10-12 16:03:50 +0300

    text2label

--- src/text2label.py
+++ src/text2label.py
@@ -4,7 +4,7 @@ from .label2id import label2id
 
 class TextToLabelModel:
     def __init__(self):
-        self.model = joblib.load("app/ml/src/text2attr.pkl")
+        self.model = joblib.load("app/ml/src/text2label.pkl")
 
     def predict(self, text) -> int:
         return label2id(self.model.predict([text])[0])
