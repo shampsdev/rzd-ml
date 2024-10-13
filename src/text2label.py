88565b31b23baa1d77a8a903b7da068194c02db9
oleg-cmd <oleg.selanta@gmail.com>
2024-10-13 10:19:14 +0300

    comments

--- src/text2label.py
+++ src/text2label.py
@@ -1,14 +1,17 @@
 import joblib
 
-
 class TextToLabelModel:
     def __init__(self):
+        # Load the trained model from a specified file
         self.model = joblib.load("app/ml/train/trained/text2label.pkl")
 
     def predict(self, text) -> int:
+        # Make a prediction for the given text and return the predicted label
         return self.model.predict([text])[0]
 
-
 if __name__ == "__main__":
+    # Create an instance of the TextToLabelModel
     model = TextToLabelModel()
+    
+    # Test the model by predicting the label for a sample text
     print(model.predict("протянуть нам 15 вагонов."))
